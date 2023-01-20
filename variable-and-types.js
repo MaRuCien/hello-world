2733a6a132ed9099720827d30f90e21d2138bd84
Maria Rubio <100109406+MaRuCien@users.noreply.github.com>
2023-01-19 23:48:54 -0300

    Add files via upload

---
+++ variable-and-types.js
@@ -0,0 +1,48 @@
+// Declaring a variable: 
+//To use a variable, you've first got to create it — more accurately, we call this declaring the variable. 
+//To do this, we type the keyword let, var or const followed by the name you want to call your variable:
+
+var myVariable;
+
+//Initializing a variable
+//Once you've declared a variable, you can initialize it with a value. 
+//You do this by typing the variable name, followed by an equals sign (=), followed by the value you want to give it. 
+//For example:
+
+var myVariable = "red";
+
+//Print Variables in the Console
+console.log(myVariable);
+
+// Updating a variable
+//Once a variable has been initialized with a value, you can change (or update) that value by giving it a different value. 
+
+myVariable = 16;
+
+myVariable = true;
+
+myVariable = false;
+
+//Variable types:
+
+//Number
+
+var myNumber = 20;
+
+//String
+
+var myString = 'I will learn JavaScript!'
+
+//Booleans
+
+var myBoolean = false
+
+//Arrays
+
+var myArray = ['Sara', 'Maria'];
+
+//Objects
+
+let myObject = { animal: 'dog', name : 'Odie', breed : 'German Shepherd' };
+
+
